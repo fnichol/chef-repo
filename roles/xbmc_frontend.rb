0498b529e5c0eaf855142a0c1475a5d5fd66c8ff
Fletcher Nichol <fnichol@nichol.ca>
2010-11-28 21:26:34 -0700

    Exec xbmc::live for xbmc_frontend role.

--- roles/xbmc_frontend.rb
+++ roles/xbmc_frontend.rb
@@ -4,7 +4,7 @@ run_list(
   "role[base]",
   "role[ubuntu]",
   "role[base_server]",
-  "recipe[xbmc::apt_repository]"
+  "recipe[xbmc::live]"
 )
 override_attributes :rvm => {
   :rubies => ["ruby-1.8.7-p302"],
