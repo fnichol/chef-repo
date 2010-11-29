3c087b21f39fad88a0a7dfa50672da9b73cb63ca
Fletcher Nichol <fnichol@nichol.ca>
2010-11-28 20:48:07 -0700

    Remove conditional logic from xbmc_frontend (guess that was wishful thinking).

--- roles/xbmc_frontend.rb
+++ roles/xbmc_frontend.rb
@@ -1,19 +1,11 @@
 name "xbmc_frontend"
 description "An Ubuntu Linux frontend for XBMC"
-
-rl = [
+run_list(
   "role[base]",
   "role[ubuntu]",
-  "role[base_server]"
-]
-
-if node[:dmi][:baseboard][:product_name] == "FMCP7A-ION-LE"
-  rl << "recipe[ubuntu_nvidia_gpu::official_repository]"
-end
-
-r1 << "recipe[xbmc::apt_repository]"
-
-run_list(*rl)
+  "role[base_server]",
+  "recipe[xbmc::apt_repository]"
+)
 override_attributes :rvm => {
   :rubies => ["1.8.7-p302"],
   :default_ruby => "1.8.7-p302"
