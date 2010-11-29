dd1d92e18e431e55d6619828cfde61f3a44a1880
Fletcher Nichol <fnichol@nichol.ca>
2010-11-28 20:43:31 -0700

    Update xbmc_frontend.

--- roles/xbmc_frontend.rb
+++ roles/xbmc_frontend.rb
@@ -14,8 +14,8 @@ end
 r1 << "recipe[xbmc::apt_repository]"
 
 run_list(*rl)
-default_attributes :rvm => {
-  :rubies => ["ruby-1.9.2-p0"],
-  :default_ruby => "ruby-1.9.2-p0"
+override_attributes :rvm => {
+  :rubies => ["1.8.7-p302"],
+  :default_ruby => "1.8.7-p302"
 }
 
