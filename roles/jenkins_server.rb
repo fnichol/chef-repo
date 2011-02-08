29bc689e5f6eb2b774229ae80960b0c4cfd184e0
fnichol <fnichol@nichol.ca>
2011-02-07 21:23:41 -0700

    Add jenkins_server role.

---
+++ roles/jenkins_server.rb
@@ -0,0 +1,24 @@
+name "jenkins_server"
+description "A web server deployment with nginx/passenger."
+run_list(
+  "role[server]",
+  "recipe[jenkins]"
+)
+override_attributes "jenkins" => {
+  "iptables_allow"  => "disable",
+  "nginx"           => { "proxy" => "enable" },
+  "server"          => {
+    "plugins"       => [
+      "deploy",
+      "disk-usage",
+      "git",
+      "github",
+      "m2release",
+      "mercurial",
+      "rake",
+      "rubyMetrics",
+      "ruby",
+      "URLSCM"
+    ]
+  }
+}
