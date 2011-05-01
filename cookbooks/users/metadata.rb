a51866dcd155bb5c06c96e5a037253bec87ae188
Fletcher Nichol <fnichol@nichol.ca>
2011-05-01 15:12:46 -0600

    Add users::default recipe to include ::sysadmins and ::normal.

--- cookbooks/users/metadata.rb
+++ cookbooks/users/metadata.rb
@@ -4,7 +4,10 @@ license          "Apache 2.0"
 description      "Creates users from a databag search"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
-recipe "users::sysadmin", "searches users data bag for sysadmins and creates users"
+
+recipe "users", "default recipe which includes both ::sysadmins and ::normal"
+recipe "users::sysadmins", "searches users data bag for sysadmins and creates users"
+recipe "users::normal", "searches users data bag for normal users and creates users"
 
 %w{ ubuntu debian redhat centos fedora freebsd}.each do |os|
   supports os
